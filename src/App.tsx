dcfffdc86844f032050835bc9ba02b38ba0a47d7
claudi_tm <ccaducarlos@gmail>
2023-07-29 09:50:48 +0200

    allgood

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,9 @@ import { Footer } from "./components/Footer";
 export function App() {
 	return (
 		<>
+			
 			<NavBar />
-			<div className="px-2 md:px-20 w-screen py-5 flex flex-col gap-64 text-zinc-800">
+			<div className="px-2 md:px-20 w-screen md:w-full py-5 flex flex-col gap-64 text-zinc-800">
 				<main className="w-full flex flex-col gap-10">
 					<Profile />
 					<Projects />
