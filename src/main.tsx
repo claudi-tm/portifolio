c8f17c72d45e678fbde6143ba76ecf2c205c68dd
Claudio Tomas <ccaducarlos@gmail.com>
2024-02-21 21:28:17 +0200

    feat: added languagem with i18next

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {App} from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.tsx";
+import "./index.css";
+import i18next from "i18next";
+import global_en from "./translations/en/global.json";
+import global_pt from "./translations/pt/global.json";
+import { I18nextProvider } from "react-i18next";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+i18next.init({
+	lng: "en", // if you're using a language detector, do not define the lng option
+	debug: true,
+	resources: {
+		en: {
+			global: global_en,
+		},
+		pt: {
+			global: global_pt,
+		},
+	},
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<I18nextProvider i18n={i18next}>
+			<App />
+		</I18nextProvider>
+	</React.StrictMode>
+);
